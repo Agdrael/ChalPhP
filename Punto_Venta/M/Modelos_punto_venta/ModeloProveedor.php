0330d647f59ad5c9845f1eba6253bca69a8f5cdc
Agdrael <rodrigojoserivera2016@gmail.com>
2024-06-25 17:05:26 -0600

    backup V6 con errores

--- Punto_Venta/M/Modelos_punto_venta/ModeloProveedor.php
+++ Punto_Venta/M/Modelos_punto_venta/ModeloProveedor.php
@@ -1,8 +1,8 @@
 <?php
 require '../conexion.php';
 $data = json_decode(file_get_contents('php://input'), true);
-
-switch($data['accion']){
+var_dump($data);
+switch ($data['accion']) {
     case 1:
         $razon_social = $data['razon_social'];
         $nombre_comercial = $data['nombre_comercial'];
@@ -13,26 +13,90 @@ switch($data['accion']){
         $departamento = $data['departamento'];
         $municipio = $data['municipio'];
         $act_economica = $data['act_economica'];
+        $nueva_act = limpiar_cadena($act_economica);
         $telefono = $data['telefono'];
-        $telefono2 = $data['telefono2'];
+        $telefono2 = $data['telefonoo'];
+        if (empty($telefono2)) {
+            $telefono2 = null;
+        }
         $NIT = $data['NIT'];
         $Dui = $data['Dui'];
         $R_fiscal = $data['R_fiscal'];
         $Giro = $data['Giro'];
         $Cuenta = $data['Cuenta'];
+        $Cuenta_sin_puntos = str_replace('.', '', $Cuenta);
         $Tamaño = $data['Tamaño'];
 
-        if(empty($razon_social)||empty($nombre_comercial)||empty($contacto)||empty($nacionalidad)|| empty($domiciliado)||empty($direccion)||empty($departamento)||empty($municipio)||empty($act_economica)||empty($telefono)||empty($telefono2)||empty($NIT)||empty($Dui)||empty($R_fiscal)||empty($Giro)||empty($Cuenta)||empty($Tamaño)){
-            echo json_encode(['status' => 'failed', 'message' => 'Campos vacios']);
-        }else{
-            try{
-                $sentencia = $pdo->prepare("INSERT INTO proveedores (nombre_proveedor,id_nacionalidad,n_comercial,contacto,domiciliado,direccion,id_departamento,municipio,id_act_economica,telefono,telefono2,nit,dui,r_fiscal,giro,cuenta,categoria) VALUES (:nombre,:,:nacionalidad, :n_comercial, :contacto, :domicialiado, :direccion, :id_depa, :municipio, :id_act_eco, :telefono, :telefono2, :nit, :dui, :r_fiscal, :giro, :cuenta, :categoria)");
-                $sentencia->bindParam(':nombre',$razon_social, PDO::PARAM_STR);
-            }catch(PDOException $x){
+        if (empty($razon_social) || empty($nombre_comercial) || empty($contacto) || empty($nacionalidad) || empty($domiciliado) || empty($direccion) || empty($departamento) || empty($municipio) || empty($nueva_act) || empty($telefono) || empty($NIT) || empty($Dui) || empty($R_fiscal) || empty($Giro) || empty($Cuenta) || empty($Tamaño)) {
+            echo json_encode(['status' => 'failed', 'message' => 'Rellene todos los campos vacios']);
+        } else {
+            try {
+                //Verificar si existe la nacionalidad
 
-            }finally{
+                $sentencia = $pdo->prepare("SELECT * FROM nacionalidades WHERE nacionalidad = :nacionalidad");
+                $sentencia->bindParam(':nacionalidad', $nacionalidad, PDO::PARAM_STR);
+                $sentencia->execute();
+                $obj_nacion = $sentencia->fetchAll(PDO::FETCH_OBJ);
 
+                if (count($obj_nacion) > 0) {
+                    //asignando id_nacionalidad
+                    $nacionalidad = $obj_nacion[0]->id;
+                } else {
+                    $sentencia2 = $pdo->prepare("INSERT INTO nacionalidades (nacionalidad) VALUES (:nacionalidad)");
+                    $sentencia2->bindParam(':nacionalidad', $nacionalidad, PDO::PARAM_STR);
+                    $sentencia2->execute();
+
+                    $sentencia3 = $pdo->prepare("SELECT * FROM nacionalidades WHERE nacionalidad = :nacionalidad");
+                    $sentencia3->bindParam('nacionalidad', $nacionalidad, PDO::PARAM_STR);
+                    $sentencia3->execute();
+                    $obj_nacion3 = $sentencia3->fetch(PDO::FETCH_OBJ);
+                    if (count($obj_nacion3) > 0) {
+                        //asignando id_nacionalidad
+                        $nacionalidad = $obj_nacion3[0]->id;
+                    } else {
+                        echo json_encode(['status' => 'failed', 'message' => 'No existe']);
+                    }
+                }
+                $sentencia4 = $pdo->prepare("SELECT id_act_economica FROM \"CAT-019\" WHERE valores = :valores");
+                $sentencia4->bindParam(':valores', $nueva_act, PDO::PARAM_STR);
+                $sentencia4->execute();
+                $obj_act = $sentencia4->fetch(PDO::FETCH_OBJ);
+                if (count($obj_act) > 0) {
+                    $nueva_act = $obj_act[0]->id;
+                }
+
+
+                $sentencia5 = $pdo->prepare("INSERT INTO proveedores (nombre_proveedor,n_comercial,contacto,id_nacionalidad,domiciliado,direccion,id_departamento,municipio,id_act_economica,telefono,telefono2,nit,dui,r_fiscal,giro,cuenta,tamaño) VALUES (:razon_social,:nombre_comercial,:contacto,:nacionalidad,:domiciliado,:direccion,:departamento,:municipio,:act_economica, :telefono, :telefono2,:NIT,:Dui,:r_fiscal,:Giro,:Cuenta_sin_puntos,:Tamaño)");
+                $sentencia5->bindParam(':razon_social', $razon_social, PDO::PARAM_STR);
+                $sentencia5->bindParam(':nombre_comercial', $nombre_comercial, PDO::PARAM_STR);
+                $sentencia5->bindParam(':contacto', $contacto, PDO::PARAM_STR);
+                $sentencia5->bindParam(':nacionalidad', $nacionalidad, PDO::PARAM_INT);
+                $sentencia5->bindParam(':domiciliado', $domiciliado, PDO::PARAM_STR_CHAR);
+                $sentencia5->bindParam(':direccion', $direccion, PDO::PARAM_STR);
+                $sentencia5->bindParam(':departamento', $departamento, PDO::PARAM_STR);
+                $sentencia5->bindParam(':act_economica', $nueva_act, PDO::PARAM_INT);
+                $sentencia5->bindParam(':telefono', $telefono, PDO::PARAM_INT);
+                $sentencia5->bindParam(':telefono2', $telefono2, PDO::PARAM_INT);
+                $sentencia5->bindParam(':NIT', $NIT, PDO::PARAM_STR);
+                $sentencia5->bindParam(':Dui', $Dui, PDO::PARAM_STR);
+                $sentencia5->bindParam(':r_fiscal', $r_fiscal, PDO::PARAM_STR);
+                $sentencia5->bindParam(':Giro', $Giro, PDO::PARAM_STR);
+                $sentencia5->bindParam(':Cuenta_sin_puntos', $Cuenta_sin_puntos, PDO::PARAM_STR);
+                $sentencia5->bindParam(':Tamaño', $Tamaño, PDO::PARAM_STR_CHAR);
+                if($sentencia5->execute()){
+                    echo json_encode(['status' => 'success', 'message' => 'Usuario guardado con exito']);
+                }
+
+            } catch (PDOException $x) {
+                echo json_encode(['status' => 'failed', 'message' =>$x->getMessage()]);
             }
         }
+        break;
 }
-echo json_encode(['status' => 'success', 'message' => 'Data received successfully']);
+
+function limpiar_cadena($cadena)
+{
+    return preg_replace('/^\d+:\s*/', '', $cadena);
+}
+
+
--- Punto_Venta/assets/js2/IngresarProveedor.js
+++ Punto_Venta/assets/js2/IngresarProveedor.js
@@ -1,23 +1,31 @@
-function Nacionalidad(){
+function Nacionalidad() {
     var otro = document.getElementById('nacionalidad').value;
     var txtnacionalidad = document.getElementById('n_pais');
 
-    if(otro == "otro")
+    if (otro == "otro")
         txtnacionalidad.disabled = false;
     else
         txtnacionalidad.disabled = true;
 }
 
-document.addEventListener('DOMContentLoaded', function (){
-    document.getElementById('guardar_proveedor').addEventListener('click',function(){
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('guardar_proveedor').addEventListener('click', function () {
         var razon_social = document.getElementById('razon_social').value;
         var nombre_comercial = document.getElementById('nombre_comercial').value;
         var contacto = document.getElementById('contacto').value;
         var nacionalidad = document.getElementById('nacionalidad').value;
-        if(nacionalidad == "otro"){
+        if (nacionalidad == "otro") {
             nacionalidad = document.getElementById('n_pais').value;
         }
-        var domiciliado = document.querySelector('input[name="Domiciliado"]:checked');
+        var domiciliado = document.getElementsByName('Domiciliado');
+        var selectedDomi = "No seleccionada";
+            for (var i = 0; i < domiciliado.length; i++) {
+                if (domiciliado[i].checked) {
+                    selectedDomi = domiciliado[i].value;
+                    break;
+                }
+            }
         var direccion = document.getElementById('direccion').value;
         var departamento = document.getElementById('departamento').value;
         var municipio = document.getElementById('municipio').value;
@@ -25,45 +33,52 @@ document.addEventListener('DOMContentLoaded', function (){
         var telefono = document.getElementById('telefono').value;
         var tele2 = document.getElementById('telefono2').value;
         var telefono2 = "Na";
-        if(tele2){
-           telefono2 = tele2.value ;
+        if (tele2.trim() !== "") {
+            telefono2 = tele2;
         }
         var NIT = document.getElementById('NIT').value;
         var Dui = document.getElementById('Dui').value;
         var R_fiscal = document.getElementById('R_fiscal').value;
         var Giro = document.getElementById('Giro').value;
         var Cuenta = document.getElementById('Cuenta').value;
-        var Tamaño = document.querySelector('input[name="Tamaño"]:checked');
+        var Tamaño = document.getElementsByName('Tamaño');
+        var selectedtamaño = "No seleccionada";
+            for (var i = 0; i < Tamaño.length; i++) {
+                if (Tamaño[i].checked) {
+                    selectedtamaño = Tamaño[i].value;
+                    break;
+                }
+            }
 
-        const data ={
+        const data = {
             accion: 1,
             razon_social: razon_social,
             nombre_comercial: nombre_comercial,
             contacto: contacto,
             nacionalidad: nacionalidad,
-            domiciliado: domiciliado,
+            domiciliado: selectedDomi,
             direccion: direccion,
             departamento: departamento,
             municipio: municipio,
             act_economica: act_economica,
             telefono: telefono,
-            telefono2: telefono2,
+            telefonoo: telefono2,
             NIT: NIT,
             Dui: Dui,
             R_fiscal: R_fiscal,
             Giro: Giro,
             Cuenta: Cuenta,
-            Tamaño: Tamaño
+            Tamaño: selectedtamaño
         }
 
-        fetch('../M/Modelos_punto_venta/ModeloProveedor.php',{
+        fetch('../M/Modelos_punto_venta/ModeloProveedor.php', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
-        .then(response => response.text())
+            .then(response => response.text())
             .then(text => {
                 console.log('Response Text:', text);
                 try {
@@ -76,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function (){
             .catch((error) => {
                 console.error('F:', error);
             });
-        
+
     });
 
 });
